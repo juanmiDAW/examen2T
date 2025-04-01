007ec6c9813b7ed542c4a38ecdefce5bec60dadc
DAW <juanmiguel.valle@iesdonana.org>
2025-04-01 17:26:03 +0200

    si se inicia con otro usuario sale que esta ocupada pero no se puede anular

--- resources/views/livewire/index-reservas.blade.php
+++ resources/views/livewire/index-reservas.blade.php
@@ -27,7 +27,9 @@
                     @for ($j = 0; $j < 5; $j++)
                         @php
                             $diaYHora = now()->startOfWeek()->addDay($j)->setTime($i, 0, 0);
-                            $reservada = \App\Models\Reserva::where('pista_id', $pista_id)->where('diaYHora', $diaYHora)->first();
+                            $reservada = \App\Models\Reserva::where('pista_id', $pista_id)
+                                ->where('diaYHora', $diaYHora)
+                                ->first();
                         @endphp
                         <td>
                             {{-- <form action="{{ route('reservas.store') }}" method="post">
@@ -35,14 +37,16 @@
 
                                 <input type="hidden" name="diaYHora" value="{{ $fecha }}">
                                 <input type="hidden" name="pista_id" value="{{ $pista_id }}"> --}}
-                                @if ($reservada)
-                                <button type="submit" wire:click="anularReserva({{$reservada->id}})"
-                                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Anular</button>    
-                                @else
-                                    
-                                <button type="submit" wire:click="crearReserva('{{$diaYHora}}')" 
+                            @if ($reservada && $reservada->user_id !== auth()->user()->id)
+                                <button type="button"
+                                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Ocupada</button>
+                            @elseif($reservada && $reservada->user_id === auth()->user()->id)
+                                <button type="submit" wire:click="anularReserva({{ $reservada->id }})"
+                                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Anular</button>
+                            @else
+                                <button type="submit" wire:click="crearReserva('{{ $diaYHora }}')"
                                     class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Reservar</button>
-                                @endif
+                            @endif
                             {{-- </form> --}}
                         </td>
                     @endfor
